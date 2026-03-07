e0db77277546b9919ec0097c95077ce5867b3887
exe.dev user <exedev@minotaur-romeo.exe.xyz>
2026-03-07 06:44:31 +0000

    design: nexavo brand tokens, card/button/badge updates, utility classes

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,9 +9,12 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
+      "rounded-xl border bg-card text-card-foreground transition-shadow duration-200",
       className
     )}
+    style={{ boxShadow: "var(--shadow-card)" }}
+    onMouseEnter={e => (e.currentTarget.style.boxShadow = "var(--shadow-hover)")}
+    onMouseLeave={e => (e.currentTarget.style.boxShadow = "var(--shadow-card)")}
     {...props}
   />
 ))
@@ -23,7 +26,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    className={cn("flex flex-col space-y-1 p-5", className)}
     {...props}
   />
 ))
@@ -57,7 +60,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("p-5 pt-0", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
@@ -67,7 +70,7 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
+    className={cn("flex items-center p-5 pt-0", className)}
     {...props}
   />
 ))
